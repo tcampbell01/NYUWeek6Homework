02a8a869d2f0301571f93c38b7b7932a7a321f88
tcampbell01 <teresa.j.campbell@gmail.com>
2025-05-25 12:57:01 -0400

    question 1 complete

--- tjc249_hw6_q1.cpp
+++ tjc249_hw6_q1.cpp
@@ -1,51 +1,47 @@
 #include <iostream>
 using namespace std;
 
-
 int fib(int n);
 
-
-
-int main() {
+int main()
+{
 
     int input;
     int fib_number = 0;
-    
 
     cout << "Please enter a positive integer:" << endl;
     cin >> input;
 
-
-    //call fib function, input becomes int n
+    // call fib function, input becomes int n
     fib_number = fib(input);
 
-    cout << fib_number << endl; //print the result 
+    cout << fib_number << endl; // print the result
 
     return 0;
-
-
 }
 
-int fib(int n) {
+int fib(int n)
+{
 
     int i;
     int one_previous = 1;
     int two_previous = 1;
     int current_n = 0;
 
-    if(n == 1 || n == 2) {
+    if (n == 1 || n == 2)
+    {
         current_n = 1;
-    } else {
+    }
+    else
+    {
 
-        for (i = 3; i <= n; i++) {
+        for (i = 3; i <= n; i++)
+        {
             current_n = one_previous + two_previous;
             two_previous = one_previous;
             one_previous = current_n;
-            
-
         }
     }
 
-    return current_n; //return the computed Fibonacci number, go back to main function line 22
-
+    return current_n; // return the computed Fibonacci number, go back to main function line 22
 }
